6069350379ddfaf04aae21c48dd2460d4b837ca5
Ben Ng <ben.ng@safelite.com>
2018-11-30 15:34:36 -0500

    got floating time headers working

--- scheduling-app/src/app/schedule/schedule/schedule.component.ts
+++ scheduling-app/src/app/schedule/schedule/schedule.component.ts
@@ -28,7 +28,6 @@ export class ScheduleComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log("Friday times: ", this.getTimesForDay(this.availableTimes[1]));
   }
 
   getTimesForDay = (dayData: DayData) => {
@@ -49,6 +48,58 @@ export class ScheduleComponent implements OnInit {
   showDayLabel = (day: DayData, i: number) => {
     if(i == 0) return day.day;
     else return null;
+  }
+
+  getLabelId = (day: DayData, i: number) => {
+    return i == 0 ? day.day.toLowerCase() + '-label' : '';
+  }
+
+  scheduleScroll = (event) => {
+    const scrollPosition = event.target.scrollTop;
+    const dayHeights = this.getAllDayHeightsInPixels();
+
+    for(let i = 1; i < dayHeights.length-1; i++) {
+      if(scrollPosition >= this.getHeightSum(dayHeights, i-1) && scrollPosition < this.getHeightSum(dayHeights, i)) {
+        this.removeStickyClasses();
+        document.getElementById(this.availableTimes[i-1].day.toLowerCase() + "-label").classList.add("sticky");
+        document.getElementsByClassName("time")[1].classList.add("sticky-helper");
+      }
+    }
+  }
+
+  removeStickyClasses = () => {
+    for(let i = 0; i < this.availableTimes.length; i++) {
+        document.getElementById(this.availableTimes[i].day.toLowerCase() + "-label").classList.remove("sticky");
+    }
+  }
+
+  getAllDayHeightsInPixels = () => {
+    const dayHeight = [];
+
+    dayHeight.push(0);
+
+    this.availableTimes.forEach((day) => {
+      dayHeight.push(this.getDayHeightInPixels(day));
+    });
+
+    dayHeight.push(1000000)
+
+    return dayHeight;
+  }
+
+  getDayHeightInPixels = (day: DayData) => {
+    const times = this.getTimesForDay(day);
+    return times.length * 30;
+  }
+
+  getHeightSum = (dayHeights: number[], index: number) => {
+    let sum = 0;
+
+    for(let i = 1; i < index+1; i++) {
+      sum += dayHeights[i];
+    }
+
+    return sum;
   }
 
 }
